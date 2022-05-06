f442e63076bcf2bb8993709ccdbf3d03a51bbdd3
lj787448952 <lj787448952@gmail.com>
2022-05-06 22:35:41 +0800

    update www

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const app = http.createServer(app);
-app.listen(process.env.PORT || 3000);
-
 const app = require('../app');
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
